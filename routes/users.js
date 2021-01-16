e944c9996d0aa1ed1eeda34cb617df9e32738190
scott-22 <scotthao65@gmail.com>
2021-01-16 12:49:08 -0500

    created create group and event api

--- routes/users.js
+++ routes/users.js
@@ -13,7 +13,7 @@ router.get('/', function(req, res, next) {
   }
 });
 
-router.get('/login', function(req, res, next) {
+router.all('/login', function(req, res, next) {
   res.redirect(`https://github.com/login/oauth/authorize?client_id=${properties.githubClientID}&scope=user`);
 });
 
@@ -44,5 +44,124 @@ router.get('/confirmlogin', function(req, res, next) {
   }).
   catch(err => res.status(500).json({ message: err.message }));
 })
+
+
+
+
+// API ENDPOINTS
+
+router.get('/creategroup', async function(req, res, next) {
+  if (!req.session.userid) res.redirect('/users/login');
+  else {
+    try {
+      let client = await pool.connect();
+      let query = await client.query(
+        'SELECT * FROM group_user WHERE userid=$1 AND groupname=$2',
+        [req.session.userid, req.body.groupname] //note this
+        );
+      if (query.rowCount > 0) throw {message: "Group with this name already exists!"};
+      query = (await client.query('INSERT INTO groups(name) VALUES($1) RETURNING *;', [req.body.groupname])).rows[0];
+      await client.query(
+        'INSERT INTO group_user(groupid, userid, groupname) VALUES($1, $2, $3)',
+        [query.id, req.session.userid, query.name]
+        );
+      await client.release();
+      res.send({message: "Group created!"});
+    }
+    catch(err) {
+      console.log(err);
+      res.send(err)
+    }
+  }
+});
+
+
+router.get('/testcreategroup', async function(req, res, next) {
+  if (!req.session.userid) res.redirect('/users/login');
+  else {
+    try {
+      let client = await pool.connect();
+      let query = await client.query(
+        'SELECT * FROM group_user WHERE userid=$1 AND groupname=$2',
+        [req.session.userid, 'test'] //note this
+        );
+      if (query.rowCount > 0) throw {message: "Group with this name already exists!"};
+      query = (await client.query('INSERT INTO groups(name) VALUES($1) RETURNING *;', ['test'])).rows[0];
+      console.log(query);
+      await client.query(
+        'INSERT INTO group_user(groupid, userid, groupname) VALUES($1, $2, $3)',
+        [query.id, req.session.userid, query.name]
+        );
+      await client.release();
+      res.send({message: "Group created!"});
+    }
+    catch(err) {
+      console.log(err);
+      res.send(err)
+    }
+  }
+});
+
+router.get('/deletegroup', async function(req, res, next) {
+
+}); // TODO: implement this
+
+
+router.get('/createevent', async function(req, res, next) {
+  if (!req.session.userid) res.redirect('/users/login');
+  else {
+    try {
+      let client = await pool.connect();
+      let query = await client.query(
+        'SELECT * FROM events WHERE groupid=$1 AND name=$2;',
+        [req.body.groupid, req.body.eventname] // note this
+        );
+      if (query.rowCount > 0) throw {message: "Event with this name already exists!"};
+      let start = (req.body.start) ? req.body.start : (await client.query('SELECT current_date;')).rows[0].current_date;
+      await client.query(
+        'INSERT INTO events(name, description, groupid, start) VALUES($1, $2, $3, $4);',
+        [req.body.eventname, req.body.description, req.body.groupid, start]
+        );
+      await client.release();
+      res.send({message: "Event created!"});
+    }
+    catch(err) {
+      console.log(err);
+      res.send(err)
+    }
+  }
+});
+
+router.get('/testcreateevent', async function(req, res, next) {
+  if (!req.session.userid) res.redirect('/users/login');
+  else {
+    try {
+      let client = await pool.connect();
+      let query = await client.query(
+        'SELECT * FROM events WHERE groupid=$1 AND name=$2;',
+        [625043619862488849, 'testevent'] // note this
+        );
+      if (query.rowCount > 0) throw {message: "Event with this name already exists!"};
+      let start = (req.body.start) ? req.body.start : (await client.query('SELECT current_date;')).rows[0].current_date;
+      console.log(start);
+      await client.query(
+        'INSERT INTO events(name, description, groupid, start) VALUES($1, $2, $3, $4);',
+        ['testevent', 'testing event for testing purposes', 625043619862488849, start]
+        );
+      await client.release();
+      res.send({message: "Event created!"});
+    }
+    catch(err) {
+      console.log(err);
+      res.send(err)
+    }
+  }
+});
+
+router.get('/deleteevent', async function(req, res, next) {
+
+}); // TODO: implement this
+
+
 
 module.exports = router;
