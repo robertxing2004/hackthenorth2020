6a05cb6480b06c5657dcfdce42e1b3f332e28235
scott-22 <scotthao65@gmail.com>
2021-01-16 09:17:18 -0500

    added database

---
+++ db.js
@@ -0,0 +1,26 @@
+var {Client, Pool} = require('pg');
+var fs = require('fs');
+var async = require('async');
+var properties = require('./properties');
+
+var config = {
+    user: 'hackathon',
+    host : 'free-tier.gcp-us-central1.cockroachlabs.cloud',
+    database: 'silly-camel-212.defaultdb',
+    password: properties.clientPassword,
+    port: 26257,
+    ssl: {
+        ca: fs.readFileSync('cc-ca.crt').toString()
+    }
+  };
+
+var pool = new Pool(config);
+/*pool.connect(async (err, client, done) => {
+    if (err) throw err;
+    var e = await client.query('SELECT * FROM test');
+    console.log(e);
+    done();
+    process.exit();
+});*/
+
+module.exports = pool;
