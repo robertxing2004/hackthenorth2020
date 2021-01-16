6c226cab13f20a19f1d9065ab5aa49040c10ebb6
Robert Xing <73909958+robertxing2004@users.noreply.github.com>
2021-01-16 14:56:06 -0500

    linked explore and profile

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var exploreRouter = require('./routes/explore');
 var properties = require('./properties');
 
 var app = express();
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/explore', exploreRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
