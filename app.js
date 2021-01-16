10a388986e09974f2a7ac14f9f2df13a4c6b2c36
Robert Xing <73909958+robertxing2004@users.noreply.github.com>
2021-01-16 17:32:16 -0500

    small changes, removed explore tab

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var exploreRouter = require('./routes/explore');
 var properties = require('./properties');
 
 var app = express();
@@ -25,7 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/explore', exploreRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
